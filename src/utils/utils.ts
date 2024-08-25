593f4c03180518599a6d4d2059de1a05af37809e
Lars Hoeg <lars.hoeg@verivox.com>
2024-08-25 22:51:36 +0200

    fix sorting

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,10 @@
 import { type TableData } from './types';
 
-export const stringToNumber = (string: string) => Number(string.replaceAll(/\D/g, ''));
-const numberStrings: (keyof TableData)[] = ['netPay', 'averageCol', 'averageTax', 'netPay'];
+export const stringToNumber = (string: string | number) => Number(String(string).replaceAll(/\D/g, ''));
+const numberStrings: (keyof TableData)[] = ['netPay', 'averageCol', 'averageTax'];
 
 export const getSortBy = (ascending: boolean, key: keyof TableData) => (a: TableData, b: TableData) => {
     const firstVal = ascending ? 1 : -1, secondVal = ascending ? -1 : 1;
-    if(numberStrings.includes(key)) return stringToNumber(a.netPay) > stringToNumber(b.netPay) ? firstVal : secondVal;
+    if(numberStrings.includes(key)) return stringToNumber(a[key]) > stringToNumber(b[key]) ? firstVal : secondVal;
     return a[key] > b[key] ? firstVal : secondVal;
 };
